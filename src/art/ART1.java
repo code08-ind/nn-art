c64150c8d73c0fba4c92c66e49ceb328537a300d
m4rtin <martin.veselovskyy@gmail.com>
2014-10-29 00:20:07 +0100

    Complete ART1 algorithm, tested with testing set from slides and compared with Zboril's program with success.

--- src/art/ART1.java
+++ src/art/ART1.java
@@ -1,42 +1,72 @@
 package art;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 /**
  * Class:
  * Author: martinvy
- * Date:   28.10.2014.
+ * Date:   28.10.2014
  * Info:
  */
 public class ART1 {
 
-    private int n = 4;  // length of input / input layer
-    private int m = 1;  // length of output layer
+    private int n = 7;  // number of input neurons / length of input
+    private int m = 1;  // number of output neurons
 
-    private double vigilance = 0.6;
+    // lists of neurons weights
+    private List<double[]> b = new ArrayList<double[]>();   // bottom-up
+    private List<int[]>    t = new ArrayList<int[]>();      // top-down
 
-    private int[][] input = {{1,0,1,0}, {1,1,1,1}, {0,1,0,0}};
-
-
-    private double[][] b = new double[m][n]; // from input to output layer
-    private int[][]    t = new int[n][m];    // from output to input layer
-    private List<int[]> tt = new ArrayList<int[]>();
+    private double vigilance = 0.7;
+    private int[][] input = {{1,1,0,0,0,0,1}, {0,0,1,1,1,1,0}, {1,0,1,1,1,1,0}, {0,0,0,1,1,1,0}, {1,1,0,1,1,1,0}};
 
 
     public ART1() {
-        tt.add(new int[n]);
-        System.out.println(tt.get(0));
-//        for (int[] t: tt)
-//        System.out.println(t.toString()+'a');
+
+        // init b weights
+        double[] wb = new double[n];
+        Arrays.fill(wb, 1.0 / (n + 1));
+        b.add(wb);
+
+        // init t weights
+        int[] wt = new int[n];
+        Arrays.fill(wt, 1);
+        t.add(wt);
+
+        write();
+    }
+
+    public void write() {
+        System.out.println("Bottom-up");
+        for (double[] i: b) {
+            for (double j : i)
+                System.out.print(" " + j);
+            System.out.println();
+        }
+
+        System.out.println("Top-down");
+        for (int[] i: t) {
+            for (int j : i)
+                System.out.print(" " + j);
+            System.out.println();
+        }
+    }
+    public String wrin(int[] in) {
+        String x = "";
+        for (int i = 0; i < n; i++)
+            x += in[i];
+        return x;
     }
 
     public void adapt() {
 
         // loop through input data
         for (int[] in: this.input) {
+
+            System.out.println("**************");
 
             // create union with output neurons to process
             Set<Integer> A = new HashSet<Integer>();
@@ -49,45 +79,70 @@ public class ART1 {
                 double y = 0, ymax = 0;
                 for (int j: A) {                // through output neurons in A
                     for (int i = 0; i < n; i++) // through all it's inputs
-                        y += b[j][i] * in[i];
+                        y += b.get(j)[i] * in[i];
                     if (y > ymax) {
                         ymax = y;
                         winner = j;
                     }
                 }
+                System.out.println("Vitazny neuron: " + winner);
 
                 // compare similarity
-                int sum_ti = 0, sum_i = 0;
+                double sum_ti = 0;
+                double sum_i  = 0;
                 for (int i = 0; i < n; i++) {
-                    sum_ti += t[i][winner] * in[i];
+                    sum_ti += t.get(winner)[i] * in[i];
                     sum_i += in[i];
                 }
+
+                System.out.println("Podobnost: " + sum_ti/sum_i);
 
                 // vigilance test
                 if (sum_ti / sum_i >= vigilance) {
 
                     // vectors are similar enough, update weights
                     for (int i = 0; i < n; i++)
-                        b[winner][i] = (t[i][winner] * in[i]) / (0.5 + sum_ti);
+                        b.get(winner)[i] = (t.get(winner)[i] * in[i]) / (0.5 + sum_ti);
                     for (int i = 0; i < n; i++)
-                        t[i][winner] = t[i][winner] * in[i];
+                        t.get(winner)[i] = t.get(winner)[i] * in[i];
 
                     A.clear();  // break loop, get next input
+
+                    System.out.println("Vstupny vektor " + wrin(in) + " priradeny k " + winner);
 
                 } else {
                     // vectors are not similar enough, remove neuron from Set
                     A.remove(winner);
 
+                    System.out.println("Vstupny vektor " + wrin(in) + " nepriradeny k " + winner);
+
                     // create new neuron
                     if (A.isEmpty()) {
                         m++;
 
+                        // calculate b weights
+                        double[] wb = new double[n];
+                        for (int i = 0; i < n; i++)
+                            wb[i] = in[i] / (0.5 + sum_i);
+                        b.add(wb);
+
+                        // set t weights same as input
+                        int[] wt = new int[n];
+                        System.arraycopy(in, 0, wt, 0, n);
+                        t.add(wt);
+
+                        System.out.println("Vstupnemu vektoru " + wrin(in) + " priradena nova trieda " + m);
                     }
                 }
+            }
+            write();
 
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            try {
+                br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
-
     }
-
 }
